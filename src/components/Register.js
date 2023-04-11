2f5c0b0668fb906e4617507da570fc670a172cfd
debrasbread <figueroa.debra@gmail.com>
2023-04-11 12:18:43 -0600

    Muestra primeras clases (elementos HTML y CSS)

---
+++ src/components/Register.js
@@ -0,0 +1,14 @@
+export function CreateRegister() {
+  //se crea una constante con el formulario
+  const formRegistro = document.createElement("form");
+  //le damos id
+  formRegistro.id = "formRegistro";
+  //le damos classname
+  formRegistro.className = "formRegistro";
+
+  
+  const inputRegistro = document.createElement("input")
+
+
+
+}
