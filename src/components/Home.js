3be2653cd0dd268d97b617b50dfb153dee41626f
debrasbread <figueroa.debra@gmail.com>
2023-04-12 22:31:11 -0600

    Muestra fondo y contenedor de vista Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,71 +1,71 @@
 export function createHome() {
   // Creación de un elemento div y asignación a la variable container
   const container = document.createElement("div");
-  container.classList.add("contenedorGeneral");
 
+  // Crear la imagen de fondo
+  const backgroundImg = document.createElement("img");
+  backgroundImg.style.position = "absolute";
+  backgroundImg.style.top = "0";
+  backgroundImg.style.left = "0";
+  backgroundImg.style.zIndex = "-1";
+  backgroundImg.style.width = "100%";
+  backgroundImg.style.height = "100%";
+  backgroundImg.style.objectFit = "cover";
+  backgroundImg.src = "../imagenes/background-mercado.jpg";
 
-  //Logo
+  // Agregar la imagen de fondo al contenedor
+  container.appendChild(backgroundImg);
+
+  // Crear el contenedor principal
+  const contenedorGeneral = document.createElement("div");
+  contenedorGeneral.id = "contenedor-General";
+
+  // Establecer el estilo del contenedor 
+  contenedorGeneral.style.display = "grid";
+  contenedorGeneral.style.gridTemplateColumns = "repeat(auto-fit, minmax(250px, 1fr))";
+  contenedorGeneral.style.gap = "20px";
+  contenedorGeneral.style.margin = "0";
+  contenedorGeneral.style.marginTop = "0";
+  contenedorGeneral.style.paddingTop = "50px";
+  contenedorGeneral.style.paddingBottom = "0";
+  contenedorGeneral.style.maxWidth = "800px";
+  contenedorGeneral.style.maxHeight = "1000px";
+  contenedorGeneral.style.minWidth = "600px"; // Revisar para responsive!
+  contenedorGeneral.style.minHeight = "900px"; // Revisar para responsive!
+  contenedorGeneral.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
+  contenedorGeneral.style.textAlign = "center"; 
+  contenedorGeneral.style.display = "flex"; 
+  contenedorGeneral.style.flexDirection = "column";
+  contenedorGeneral.style.justifyContent = "center"; 
+  contenedorGeneral.style.alignItems = "center"; 
+  //contenedorGeneral.style.borderRadius = "3%";
+  
+
+  // Agregar el contenedor al contenedor principal
+  container.appendChild(contenedorGeneral);
+
+  // Logo
   const logo = document.createElement("img");
   logo.classList.add("logoMarchantes");
   logo.src = "../imagenes/logo-marchantes.png";
+  contenedorGeneral.appendChild(logo);
 
-  container.appendChild(logo);
-
-// Imagen de fondo
-/*
-const imgFondo = document.createElement("img");
-imgFondo.classList.add("imagenFondo");
-imgFondo.src = "../imagenes/marchantes-fondo-home.jpg";
-
-container.appendChild(imgFondo);
-*/
-
-
-/*
-  // Creación de un elemento h1 y asignación a la variable title
-  const title = document.createElement("h1");
-  // Creación de un elemento p y asignación a la variable text
-  const text = document.createElement("p");
-
-*/
-
-  // Creación de un elemento button y asignación a la variable button
+  // Botón "Iniciar sesión"
   const iniciarSesionBtn = document.createElement("button");
+  iniciarSesionBtn.classList.add("iniciarSesionBtn");
+  iniciarSesionBtn.textContent = "Iniciar sesión";
+  contenedorGeneral.appendChild(iniciarSesionBtn);
 
-  iniciarSesionBtn.classList.add = "iniciarSesionBtn";
+// Texto e hipervínculo "Regístrate"
+const registroLink = document.createElement("p");
+registroLink.innerHTML = '¿No tienes una cuenta? <a href="#">Regístrate</a>.';
+registroLink.style.margin = "0";
+registroLink.style.paddingTop = "3px";
+registroLink.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; 
+registroLink.style.color = "white"; 
+contenedorGeneral.appendChild(registroLink);
 
-  container.appendChild(iniciarSesionBtn);
-
-
-  // Asignación del texto "Marchantes" al elemento h1
-  //title.textContent = "Marchantes";
-
-  // Asignación de una cadena vacía al elemento p (no se establece ningún texto)
-  //text.textContent =
-  
-  // Asignación del texto "Iniciar sesión" al elemento button
-  iniciarSesionBtn.textContent = "Iniciar sesión"; 
- 
-
-  /*
-  // Agregado del elemento h1 al elemento div
-  container.appendChild(title);
-  // Agregado del elemento p al elemento div
-  container.appendChild(text);
-  
-
-  // Establecimiento del color azul para el elemento h1
-  title.style.color = "blue";
-  // Establecimiento del color verde para el fondo del elemento button
-  button.style.backgroundColor = "green";
-*/
-  
 
   // Retorna el elemento div (con sus elementos hijos dentro)
   return container;
 }
-
-
-
-
-
