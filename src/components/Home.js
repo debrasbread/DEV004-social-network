4e3de2104211f40abaa0e22879c90bce80a59541
debrasbread <figueroa.debra@gmail.com>
2023-04-13 12:47:57 -0600

    Muestra estilos de Home sólo en CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,46 +2,16 @@ export function createHome() {
   // Creación de un elemento div y asignación a la variable container
   const container = document.createElement("div");
 
-  // Crear la imagen de fondo
+  // Background
   const backgroundImg = document.createElement("img");
-  backgroundImg.style.position = "absolute";
-  backgroundImg.style.top = "0";
-  backgroundImg.style.left = "0";
-  backgroundImg.style.zIndex = "-1";
-  backgroundImg.style.width = "100%";
-  backgroundImg.style.height = "100%";
-  backgroundImg.style.objectFit = "cover";
+  backgroundImg.classList.add("backgroundImg");
   backgroundImg.src = "../imagenes/background-mercado.jpg";
-
-  // Agregar la imagen de fondo al contenedor
   container.appendChild(backgroundImg);
 
-  // Crear el contenedor principal
+  // Contenedor principal
   const contenedorGeneral = document.createElement("div");
   contenedorGeneral.id = "contenedor-General";
-
-  // Establecer el estilo del contenedor 
-  contenedorGeneral.style.display = "grid";
-  contenedorGeneral.style.gridTemplateColumns = "repeat(auto-fit, minmax(250px, 1fr))";
-  contenedorGeneral.style.gap = "20px";
-  contenedorGeneral.style.margin = "0";
-  contenedorGeneral.style.marginTop = "0";
-  contenedorGeneral.style.paddingTop = "50px";
-  contenedorGeneral.style.paddingBottom = "0";
-  contenedorGeneral.style.maxWidth = "800px";
-  contenedorGeneral.style.maxHeight = "1000px";
-  contenedorGeneral.style.minWidth = "600px"; // Revisar para responsive!
-  contenedorGeneral.style.minHeight = "900px"; // Revisar para responsive!
-  contenedorGeneral.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
-  contenedorGeneral.style.textAlign = "center"; 
-  contenedorGeneral.style.display = "flex"; 
-  contenedorGeneral.style.flexDirection = "column";
-  contenedorGeneral.style.justifyContent = "center"; 
-  contenedorGeneral.style.alignItems = "center"; 
-  //contenedorGeneral.style.borderRadius = "3%";
-  
-
-  // Agregar el contenedor al contenedor principal
+  contenedorGeneral.classList.add("contenedorGeneral");
   container.appendChild(contenedorGeneral);
 
   // Logo
@@ -56,15 +26,11 @@ export function createHome() {
   iniciarSesionBtn.textContent = "Iniciar sesión";
   contenedorGeneral.appendChild(iniciarSesionBtn);
 
-// Texto e hipervínculo "Regístrate"
-const registroLink = document.createElement("p");
-registroLink.innerHTML = '¿No tienes una cuenta? <a href="#">Regístrate</a>.';
-registroLink.style.margin = "0";
-registroLink.style.paddingTop = "3px";
-registroLink.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; 
-registroLink.style.color = "white"; 
-contenedorGeneral.appendChild(registroLink);
-
+  // Texto e hipervínculo "Regístrate"
+  const registroLink = document.createElement("p");
+  registroLink.classList.add("registroLink");
+  registroLink.innerHTML = '¿No tienes una cuenta? <a href="#">Regístrate</a>.';
+  contenedorGeneral.appendChild(registroLink);
 
   // Retorna el elemento div (con sus elementos hijos dentro)
   return container;
