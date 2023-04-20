f0e71d0809eab18a68ea2d7803a077f01df6150f
debrasbread <figueroa.debra@gmail.com>
2023-04-20 14:54:35 -0600

    Primeros elementos Feed

--- src/components/views/feed.js
+++ src/components/views/feed.js
@@ -1,2 +1,36 @@
 export function feed() {
+
+ // Creación de main y asignación a variable home
+ const containerFeed = document.createElement("main");
+
+ // Contenedor de elementos
+ const contenedorGeneralFeed = document.createElement("main");
+ contenedorGeneralFeed.id = "contenedor-GeneralF";
+ contenedorGeneralFeed.classList.add("contenedorGeneralF");
+ containerFeed.appendChild(contenedorGeneralFeed);
+
+ // Logo
+ const logoFeed = document.createElement("img");
+ logoFeed.classList.add("logoF");
+ logoFeed.src = "../imagenes/logo-marchantes.png";
+ contenedorGeneralFeed.appendChild(logoFeed);
+
+ // Post
+
+ // Footer
+ const footerFeed = document.createElement("footer");
+ footerFeed.id = "footer-Feed";
+ footerFeed.classList.add("footerF");
+ footerFeed.textContent = "Marchantes, 2023";
+ contenedorGeneralFeed.appendChild(footerFeed);
+
+ // Retorna el contenedor principal
+ return containerFeed;
+
+
+
+
+
+
+
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import { addRoutes, onNavigate } from "./lib/router/index.js";
-import { home } from "./components/home.js";
-import { login } from "./components/login.js";
-import { register } from "./components/register.js";
-//import { feed } from './components/feed.js';
+import { home } from "./components/views/home.js";
+import { login } from "./components/views/login.js";
+import { register } from "./components/views/register.js";
+import { feed } from "./components/views/feed.js";
 //import { loginGoogle } from "./lib/autenticar.js";
 
 addRoutes({
   "/": home,
   "/login": login,
   "/register": register,
-  //"/feed": feed,
+  "/feed": feed,
   //"/loginGoogle": loginGoogle,
+  //"/post": post, // El post podría ir dentro del feed y ser una especie de ventana emergente (?)
 });
 
 window.onload = () => {
